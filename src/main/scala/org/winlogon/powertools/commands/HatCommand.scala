86967cddfb43d95c17012032b954faaf6cf47009
winlogon <walker84837@gmail.com>
2025-01-20 10:23:24 +0100

    feat: implement /hat command

--- src/main/scala/org/winlogon/powertools/commands/HatCommand.scala
+++ src/main/scala/org/winlogon/powertools/commands/HatCommand.scala
@@ -1,12 +1,24 @@
 package org.winlogon.powertools.commands
 
 import org.bukkit.command.{Command, CommandExecutor, CommandSender}
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.winlogon.powertools.ChatFormatting
 
 class HatCommand extends CommandExecutor {
   override def onCommand(
     sender: CommandSender, command: Command, label: String, args: Array[String]
   ): Boolean = {
-    sender.sendMessage("hat command")
-    true
+    if (!sender.isInstanceOf[Player]) {
+      sender.sendMessage(ChatFormatting.apply("<#F93822>Error&7: This command can only be used by players."))
+      return true
+    }
+    val player = sender.asInstanceOf[Player]
+    val itemInHand = player.getInventory().getItemInMainHand()
+    player.getInventory().setHelmet(itemInHand)
+    player.getInventory().setItemInMainHand(null)
+    player.updateInventory()
+    player.sendMessage(ChatFormatting.apply("&7Your held item is now &3on your head!"))
+    return true
   }
 }
