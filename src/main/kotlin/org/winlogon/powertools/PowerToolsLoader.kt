5b7134575412321489b5985b88175a7045ebab25
winlogon <walker84837@gmail.com>
2025-07-27 06:21:18 +0200

    chore: make lamp runtime dep using paper loader

--- src/main/kotlin/org/winlogon/powertools/PowerToolsLoader.kt
+++ src/main/kotlin/org/winlogon/powertools/PowerToolsLoader.kt
@@ -3,6 +3,7 @@ package org.winlogon.powertools
 import io.papermc.paper.plugin.loader.PluginClasspathBuilder
 import io.papermc.paper.plugin.loader.PluginLoader
 import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver
+
 import org.eclipse.aether.artifact.DefaultArtifact
 import org.eclipse.aether.graph.Dependency
 import org.eclipse.aether.repository.RemoteRepository
@@ -11,28 +12,34 @@ class PowerToolsLoader : PluginLoader {
     override fun classloader(classpathBuilder: PluginClasspathBuilder) {
         val resolver = MavenLibraryResolver()
 
-        resolver.addRepository(
-            RemoteRepository.Builder(
-                "central",
-                "default",
-                MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR
-            ).build()
+        val resolvers = mapOf(
+            "central" to MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR,
+            "winlogon-code" to "https://maven.winlogon.org/releases"
         )
 
-        resolver.addRepository(
-            RemoteRepository.Builder(
-                "winlogon-code",
-                "default",
-                "https://maven.winlogon.org/releases"
-            ).build()
-        )
-
-        resolver.addDependency(
-            Dependency(
-                DefaultArtifact("org.winlogon:retrohue:0.1.1"),
-                null
+        resolvers.forEach { (name, url) ->
+            resolver.addRepository(
+                RemoteRepository.Builder(name, "default", url).build()
             )
+        }
+
+        val lampVersion = "4.0.0-rc.12"
+        val dependencies = mapOf(
+            "org.winlogon:retrohue" to "0.1.1",
+            "org.winlogon:asynccraftr" to "0.1.1",
+            "io.github.revxrsal:lamp.common" to lampVersion,
+            "io.github.revxrsal:lamp.bukkit" to lampVersion,
+            "io.github.revxrsal:lamp.brigadier" to lampVersion
         )
+
+        dependencies.forEach { (artifactId, version) ->
+            resolver.addDependency(
+                Dependency(
+                    DefaultArtifact("$artifactId:$version"),
+                    null
+                )
+            )
+        }
 
         classpathBuilder.addLibrary(resolver)
     }
