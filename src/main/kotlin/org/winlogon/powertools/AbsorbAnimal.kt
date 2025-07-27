02fc92c2b74321ebf38b41487a3c8f04ea1becb1
winlogon <walker84837@gmail.com>
2025-07-27 06:20:37 +0200

    chore: trim trailing whitespace

--- src/main/kotlin/org/winlogon/powertools/AbsorbAnimal.kt
+++ src/main/kotlin/org/winlogon/powertools/AbsorbAnimal.kt
@@ -18,7 +18,7 @@ class AbsorbAnimal(private val plugin: PowerToolsPlugin) {
             return
         }
 
-        if (targetEntity is Tameable && targetEntity.isTamed && 
+        if (targetEntity is Tameable && targetEntity.isTamed &&
             targetEntity.owner?.uniqueId == player.uniqueId) {
             absorbPet(player, targetEntity)
         } else {
@@ -29,32 +29,32 @@ class AbsorbAnimal(private val plugin: PowerToolsPlugin) {
     private fun absorbPet(player: Player, pet: Tameable) {
         val item = ItemStack(Material.STICK)
         val meta = item.itemMeta
-        
+
         val entityType = pet.type
         val displayName = fmt("<dark_aqua>Absorbed ${entityType.name.lowercase().replace('_', ' ')}")
         meta.displayName(displayName)
-        
+
         val lore = listOf(
             fmt("<gray>Right click to spawn</gray>"),
             fmt("<gray>Pet ID: <dark_gray>${pet.uniqueId}</dark_gray></gray>")
         )
         meta.lore(lore)
-        
+
         val pdc = meta.persistentDataContainer
         val entityTypeKey = NamespacedKey(plugin, "absorbed_entity_type")
         pdc.set(entityTypeKey, PersistentDataType.STRING, entityType.name)
-        
+
         val nbtKey = NamespacedKey(plugin, "absorbed_nbt")
         pdc.set(nbtKey, PersistentDataType.STRING, "idk")
-        
+
         item.itemMeta = meta
-        
+
         pet.remove()
         player.inventory.addItem(item)
         player.sendMessage(fmt("<dark_green>Successfully absorbed your pet!"))
     }
-    
+
     private fun fmt(s: String): Component {
-        return plugin.fmt(s)
+        return ChatFormatting.colorConverter.convertToComponent(s, '&')
     }
 }
